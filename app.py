8a2bf6c7b88a14993cdfc7d4ab7f303e11a36dce
Paildra <paildra33@gmail.com>
2026-09-02 09:37:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ if modalita == "👤 Diario Personale (Journaling)":
             try:
                 response = client.chat.completions.create(
                   model="llama-3.3-70b-versatile", 
-                  messages=chat_da_analizzare,
+                  messages=st.session_state.journal_messages,,
                   temperature=0.1,
                   response_format={"type": "json_object"}
                 )
